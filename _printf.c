427b60dbb8730c4bd12f5e99b5c64ba293a14ef8
mustapha ibrahim <iamustapha213@gmaiil.com>
2023-08-22 01:28:29 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,50 +1,16 @@
 #include "main.h"
 
+/**
+  * _printf - a function that replicate printf
+  * @format: code
+  * Return: length
+  */
+
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    va_start(ap, format);
+	va_list arg;
+	va_start(arg, format);
 
-    int i = 0;
-    int num = 0;
-
-    while (format[i])
-    {
-        if (format[i] == '%' && format[i + 1] != '\0')
-        {
-            switch (format[i + 1])
-            {
-            case 'c':
-                _putchar(va_arg(ap, int));
-                break;
-            case 's':
-            {
-                char *str = va_arg(ap, char *);
-                if (str != NULL)
-                {
-                    num += print_s(str);
-                }
-                else
-                {
-                    printf("(nil)");
-                }
-                break;
-            }
-            default:
-                _putchar(format[i]); // Print non-format characters directly
-            }
-            i++; // Skip the format specifier character
-        }
-        else
-        {
-            _putchar(format[i]);
-        }
-
-        i++;
-    }
-
-    va_end(ap);
-    printf("\n");
-    return (num);
+	int i = 0;
+	int num = 0;
 }
-
