5ae28df054263b523eb1f88ce55769cd38447b6a
mustapha ibrahim <iamustapha213@gmaiil.com>
2023-08-22 01:20:14 -0700

    done working on the sttring and char

--- _printf.c
+++ _printf.c
@@ -1,17 +1,50 @@
 #include "main.h"
-#include <stdarg.h>
 
 int _printf(const char *format, ...)
 {
-    va_list arg;
+    va_list ap;
+    va_start(ap, format);
 
-    va_start(arg, format);
+    int i = 0;
+    int num = 0;
 
-    int len = format(format, arg);
+    while (format[i])
+    {
+        if (format[i] == '%' && format[i + 1] != '\0')
+        {
+            switch (format[i + 1])
+            {
+            case 'c':
+                _putchar(va_arg(ap, int));
+                break;
+            case 's':
+            {
+                char *str = va_arg(ap, char *);
+                if (str != NULL)
+                {
+                    num += print_s(str);
+                }
+                else
+                {
+                    printf("(nil)");
+                }
+                break;
+            }
+            default:
+                _putchar(format[i]); // Print non-format characters directly
+            }
+            i++; // Skip the format specifier character
+        }
+        else
+        {
+            _putchar(format[i]);
+        }
 
-    va_end(arg);
+        i++;
+    }
 
-    return (len);
-
+    va_end(ap);
+    printf("\n");
+    return (num);
 }
 
